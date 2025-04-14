0ac6ba380e5a6680fc61b711ec1d0f90e2569ff0
foobar <59284168+bingchris@users.noreply.github.com>
2025-04-14 19:18:43 +0200

    Update home.php

--- home.php
+++ home.php
@@ -7,17 +7,8 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.htm");
     exit();
 }
-
-// Database connection
-$host = 'mysql.ct8.pl';
-$user = 'm42569_gaouser';
-$pass = 'Android 8.1.0';
-$db = 'm42569_openguild';
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
+// gaouser dont make the mistake again
+require 'db_connection.php';
 
 // Get user's guilds
 $userId = $_SESSION['user_id'];
